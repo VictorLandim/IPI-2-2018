93d8ceedac5a64ab5326ff250a1e8f25733b4627
Victor Landim <victorlandim5@gmail.com>
2018-09-01 22:40:35 +0000

    Assignment 1 progress

---
+++ assignment-1/script.js
@@ -0,0 +1,161 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const select = s => document.querySelector(s);
+
+    const status = select('.status');
+    const image = select('.image');
+    const fileInput = select('.file-input');
+    const button = select('.button');
+    const spinner = select('.spinner');
+    const canvas = select('#canvas');
+
+    status.innerHTML = 'Open CV loaded successfully';
+    status.classList.remove('red');
+    status.classList.add('green');
+
+    fileInput.addEventListener('change', e => {
+        image.src = URL.createObjectURL(e.target.files[0]);
+    });
+
+    button.onclick = () => {
+        spinner.style.display = 'block';
+        canvas.style.display = 'none';
+        fileInput.click();
+    };
+
+    image.onload = () => {
+        let mat = cv.imread(image);
+        let displayMat = cv.imread(image);
+
+        spinner.style.display = 'none';
+        canvas.style.display = 'block';
+
+        cv.imshow('canvas', displayMat);
+
+        const origin = {
+            x: 260,
+            y: 415
+        };
+
+        const dest = {
+            x: 815,
+            y: 1000
+        };
+
+        let current = origin;
+        let bestDistance = euclideanDistance(current, dest);
+
+        toGrayScale(mat);
+        toGrayScale(displayMat);
+
+        //or while current != dest
+        while (euclideanDistance(current, dest) !== 0) {
+            // for (var i = 0; i < 10000; i++) {
+            let eightNeightbours = getEightNeightbours(mat, current.x, current.y);
+
+            // console.log('%d,    %d,    %d', eightNeightbours[0].r, eightNeightbours[1].r, eightNeightbours[2].r);
+            // console.log('%d,    .      %d', eightNeightbours[3].r, eightNeightbours[4].r);
+            // console.log('%d,    %d,    %d', eightNeightbours[5].r, eightNeightbours[6].r, eightNeightbours[7].r);
+            // console.log('-------------');
+
+            eightNeightbours = eightNeightbours.map(e => ({
+                ...e,
+                distance: euclideanDistance(e, dest)
+            }));
+
+            //sort by distance
+            eightNeightbours.sort((a, b) => a.distance - b.distance);
+
+            //3 closest pixels
+            const candidates = eightNeightbours.slice(0, 3);
+
+            //sort by gray level
+            candidates.sort((a, b) => a.r - b.r);
+
+            //lowest distance and lowest gray level
+            // TODO
+            // if (candidates[0].distance <= bestDistance) {
+            //     current = candidates[0];
+            // } else {
+            //     current = candidates[Math.round(Math.random() + 1)];
+            // }
+            current = candidates[0];
+            bestDistance = current.distance;
+
+            setPixel(displayMat, current.x, current.y, BLACK);
+        }
+
+        setPixel(displayMat, origin.x, origin.y, CYAN);
+        setPixel(displayMat, dest.x, dest.y, CYAN);
+
+        cv.imshow('canvas', displayMat);
+        mat.delete();
+        displayMat.delete();
+    };
+});
+
+const RED = {
+    r: 255,
+    g: 0,
+    b: 0,
+    a: 255
+};
+
+const CYAN = {
+    r: 0,
+    g: 255,
+    b: 255,
+    a: 255
+};
+
+const BLACK = {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 255
+};
+
+const getPixel = (mat, x, y) => ({
+    x,
+    y,
+    r: mat.data[x * mat.cols * mat.channels() + y * mat.channels()],
+    g: mat.data[x * mat.cols * mat.channels() + y * mat.channels() + 1],
+    b: mat.data[x * mat.cols * mat.channels() + y * mat.channels() + 2],
+    a: mat.data[x * mat.cols * mat.channels() + y * mat.channels() + 3]
+});
+
+const setPixel = (mat, x, y, { r, g, b, a }) => {
+    mat.data[x * mat.cols * mat.channels() + y * mat.channels()] = r;
+    mat.data[x * mat.cols * mat.channels() + y * mat.channels() + 1] = g;
+    mat.data[x * mat.cols * mat.channels() + y * mat.channels() + 2] = b;
+    mat.data[x * mat.cols * mat.channels() + y * mat.channels() + 3] = a;
+};
+
+const toGrayScale = mat => {
+    const rows = mat.rows;
+    const cols = mat.cols;
+
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            let pixel = getPixel(mat, i, j);
+            const y = pixel.r * 0.299 + pixel.g * 0.587 + pixel.b * 0.114;
+            pixel.r = pixel.g = pixel.b = y;
+
+            setPixel(mat, i, j, pixel);
+        }
+    }
+};
+
+const getEightNeightbours = (mat, x, y) => [
+    getPixel(mat, x - 1, y - 1),
+    getPixel(mat, x, y - 1),
+    getPixel(mat, x + 1, y - 1),
+    getPixel(mat, x - 1, y),
+    getPixel(mat, x + 1, y),
+    getPixel(mat, x - 1, y + 1),
+    getPixel(mat, x, y + 1),
+    getPixel(mat, x + 1, y + 1)
+];
+
+const euclideanDistance = (a, b) => Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
